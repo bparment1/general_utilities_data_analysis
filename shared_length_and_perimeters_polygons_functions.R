1867da42f38fad7b8f6e9e595c2aa974c762ed30
bparment1 <benoit.parmentier@gmail.com>
2017-06-02 14:30:00 -0400

    initial commit for shared boundaries functions

---
+++ shared_length_and_perimeters_polygons_functions.R
@@ -0,0 +1,102 @@
+############### SESYNC Research Support: Urbanization Impact on Biodiversity ########## 
+##
+## 
+## DATE CREATED: 06/02/2017
+## DATE MODIFIED: 06/02/2017
+## AUTHORS: Benoit Parmentier 
+## Version: 1
+## PROJECT: Urbanization impact on biodiversity
+## ISSUE: 
+## TO DO:
+##
+## COMMIT: initial commit
+##
+## Links to investigate:
+#https://gis.stackexchange.com/questions/119993/convert-line-shapefile-to-raster-value-total-length-of-lines-within-cell
+#https://edzer.github.io/sfr/articles/sfr.html
+#https://geographicdatascience.com/2017/01/06/first-impressions-from-sf-the-simple-features-r-package/
+
+###################################################
+#
+
+###### Library used
+
+library(gtools)                              # loading some useful tools 
+library(sp)                                  # Spatial pacakge with class definition by Bivand et al.
+library(spdep)                               # Spatial pacakge with methods and spatial stat. by Bivand et al.
+library(rgdal)                               # GDAL wrapper for R, spatial utilities
+library(gdata)                               # various tools with xls reading, cbindX
+library(rasterVis)                           # Raster plotting functions
+library(parallel)                            # Parallelization of processes with multiple cores
+library(maptools)                            # Tools and functions for sp and other spatial objects e.g. spCbind
+library(maps)                                # Tools and data for spatial/geographic objects
+library(plyr)                                # Various tools including rbind.fill
+library(spgwr)                               # GWR method
+library(rgeos)                               # Geometric, topologic library of functions
+library(gridExtra)                           # Combining lattice plots
+library(colorRamps)                          # Palette/color ramps for symbology
+library(ggplot2)
+
+###### Functions used in this script
+
+intersect_poly_length_fun <- function(poly_sp1,poly_sp2){
+  intersection_line <- gIntersection(poly_sp1,poly_sp2)
+  length_val <- SpatialLinesLengths(intersection_line)
+  intersect_obj <- list(length_val,intersection_line)
+  return(intersect_obj)
+}
+
+calculate_shared_length <- function(i,poly_sp,poly_nb){
+  #
+  #
+  
+  poly_sp_ref_selected <- poly_sp[i,] # reference polygon under consideration for which neighbours are compared to
+  poly_nb_selected <- poly_nb[[i]] #list of neighbour for the ref poly selected
+  list_polygons_neighbors <- lapply(poly_nb_selected,FUN=function(j){poly_sp[j,]})
+  names(list_polygons_neighbors) <- poly_nb_selected
+  
+  #debug(interesect_poly_length_fun)
+  
+  list_lines_shared <- lapply(list_polygons_neighbors,FUN=interesect_poly_length_fun,poly_sp2=poly_sp_ref_selected)
+  return(list_lines_shared)
+}
+
+calculate_shared_boundaries_polygons <- function(poly_sp,poly_nb=NULL,edges=F,num_cores=1,out_dir=".",out_suffix=""){
+  #Calculate the length of shared edged between neighbours of a spatial polygons data frame object
+  #
+  ##INPUTS:
+  #1) poly_sp
+  #2) list_nb
+  #3) num_cores
+  #4) edges: if edges is TRUE keep spatialLines objects corresponding to share boundaries between polygons
+  #4) out_dir
+  #5) out_suffix
+  
+  ####### Begin function ####
+  
+  if(is.null(poly_nb)){
+    poly_nb <- poly2nb(poly_sp)
+  }
+  
+  no_poly <- length(poly_sp) #number of polygons features in the dataset
+  list_lines_shared <- mclapply(1:no_poly,
+                                    FUN=calculate_shared_length,
+                                    poly_sp = poly_sp,
+                                    poly_nb = poly_nb,
+                                    mc.preschedule=FALSE,
+                                    mc.cores = num_cores)
+  #debug(calculate_shared_length)
+  test_lines_shared <- calculate_shared_length(11,
+                                poly_sp = poly_sp,
+                                poly_nb = poly_nb )
+  
+  gInter_poly <- gIntersection(test_poly1,test_poly2)
+  SpatialLinesLengths(gInter_poly)
+  
+  return()
+}
+
+
+
+################## END OF SCRIPT #########################
+
