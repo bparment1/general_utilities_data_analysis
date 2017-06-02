b2199d6f8571a3719ff16b4193af49df6a85cfb0
bparment1 <benoit.parmentier@gmail.com>
2017-06-02 14:30:00 -0400

    modifying outputs of list to data.frame of connectivity and list spatial obj

--- shared_length_and_perimeters_polygons_functions.R
+++ shared_length_and_perimeters_polygons_functions.R
@@ -1,5 +1,6 @@
 ############### SESYNC Research Support: Urbanization Impact on Biodiversity ########## 
 ##
+## Calculating shared length of boudaries of neighbors. This is useful for spatial analysis.
 ## 
 ## DATE CREATED: 06/02/2017
 ## DATE MODIFIED: 06/02/2017
@@ -9,7 +10,7 @@
 ## ISSUE: 
 ## TO DO:
 ##
-## COMMIT: dealing with points and line connectivity among neighbours
+## COMMIT: modifying outputs of list to data.frame of connectivity and list spatial obj
 ##
 ## Links to investigate:
 #https://gis.stackexchange.com/questions/119993/convert-line-shapefile-to-raster-value-total-length-of-lines-within-cell
@@ -48,15 +49,24 @@ intersect_poly_length_fun <- function(poly_sp1,poly_sp2){
   #note that sp object can be points or lines, if points then lenght is zero
   if(inherits(intersection_sp,"SpatialLines")){
     length_val <- SpatialLinesLengths(intersection_sp)
-  }else{
+  }
+  if(inherits(intersection_sp,"SpatialPoints")){
     length_val <- 0
   }
+  
+  ### Add overlap if polygon
+  #length_val <- NA
+  #area_val <- ...
+  
   intersect_obj <- list(length_val,intersection_sp)
   
   return(intersect_obj)
 }
 
 calculate_shared_length <- function(i,poly_sp,poly_nb){
+  #
+  #
+  
   poly_sp_ref_selected <- poly_sp[i,] # reference polygon under consideration for which neighbours are compared to
   poly_nb_selected <- poly_nb[[i]] #list of neighbour for the ref poly selected
   list_polygons_neighbors <- lapply(poly_nb_selected,FUN=function(j){poly_sp[j,]})
@@ -65,7 +75,23 @@ calculate_shared_length <- function(i,poly_sp,poly_nb){
   #debug(interesect_poly_length_fun)
   
   list_lines_shared <- lapply(list_polygons_neighbors,FUN=intersect_poly_length_fun,poly_sp2=poly_sp_ref_selected)
-  return(list_lines_shared)
+  #browser()
+  
+  #list_lines_shared
+  
+  #browser()
+  ### make data.frame with length and id of neighbours
+  nb_df <- lapply(list_lines_shared,function(x){x[[1]]})
+  nb_df <- do.call(rbind,nb_df)
+  nb_df <- as.data.frame(nb_df)
+  names(nb_df) <- "length"
+  nb_df$id <- rownames(nb_df)
+  rownames(nb_df) <- NULL
+  ### Extract sp object resulting from intersection
+  list_nb_sp <- lapply(list_lines_shared,function(x){x[[2]]})
+  
+  shared_nb_obj <- list(nb_df=nb_df,list_nb_sp=list_nb_sp)
+  return(shared_nb_obj)
 }
 
 calculate_shared_boundaries_polygons <- function(poly_sp,poly_nb=NULL,edges=F,num_cores=1,out_dir=".",out_suffix=""){
@@ -91,14 +117,15 @@ calculate_shared_boundaries_polygons <- function(poly_sp,poly_nb=NULL,edges=F,nu
   #                                             poly_nb = poly_nb )
   
   no_poly <- length(poly_sp) #number of polygons features in the dataset
-  list_lines_shared <- mclapply(1:no_poly,
-                                    FUN=calculate_shared_length,
-                                    poly_sp = poly_sp,
-                                    poly_nb = poly_nb,
-                                    mc.preschedule=FALSE,
-                                    mc.cores = num_cores)
+  list_shared_nb_obj <- mclapply(1:no_poly,
+                                 FUN=calculate_shared_length,
+                                 poly_sp = poly_sp,
+                                 poly_nb = poly_nb,
+                                 mc.preschedule=FALSE,
+                                 mc.cores = num_cores)
   
-  return(list_lines_shared)
+  
+  return(list_shared_nb_obj)
 }
 
 
