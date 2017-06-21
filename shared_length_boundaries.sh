aadfa5869748074f90019aa7044761fb439840de
bparment1 <benoit.parmentier@gmail.com>
2017-06-21 14:30:00 -0400

    adding out_suffix in shell argument for SLURM and testing bug for biome shapefile

--- shared_length_boundaries.sh
+++ shared_length_boundaries.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=shared_boundaries_test    # Job name
 #SBATCH --mail-type=ALL                          # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=bparmentier@sesync.org       # Where to send mail
-#SBATCH --time=10:50:00                          # Time limit hrs:min:sec
+#SBATCH --time=01:00:00                          # Time limit hrs:min:sec
 #SBATCH --output=shared_boundaries_test_%A_%a.out   # Standard output from console
 #SBATCH --error=shared_boundaries_test_%A_%a.err   # Error log 
 #SBATCH --partition=sesynctest        # queue name, this is for debugging and testing
@@ -13,6 +13,8 @@ pwd; hostname; date
 echo "Running shared boundaries code for testing"
  
 INFILE_NAME=Biomes_disolv.shp
+OUT_SUFFIX=urbanization_effects_biodiversity_06212017
 export INFILE_NAME
+export OUT_SUFFIX
 
-Rscript --vanilla /nfs/bparmentier-data/Data/projects/urbanization_effects_on_biodiversity/scripts/shared_length_and_perimeters_polygons_06202017.R
+Rscript --vanilla /nfs/bparmentier-data/Data/projects/urbanization_effects_on_biodiversity/scripts/shared_length_and_perimeters_polygons_06212017.R
