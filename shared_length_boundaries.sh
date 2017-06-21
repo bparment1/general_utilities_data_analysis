559d6ce8e9119da9abf9d092ea6785bd0ea98e39
bparment1 <benoit.parmentier@gmail.com>
2017-06-21 14:30:00 -0400

    testing slurm job on biome shapefile to find bug

--- shared_length_boundaries.sh
+++ shared_length_boundaries.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=shared_boundaries_test    # Job name
 #SBATCH --mail-type=ALL                          # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=bparmentier@sesync.org       # Where to send mail
-#SBATCH --time=00:50:00                          # Time limit hrs:min:sec
+#SBATCH --time=10:50:00                          # Time limit hrs:min:sec
 #SBATCH --output=shared_boundaries_test_%A_%a.out   # Standard output from console
 #SBATCH --error=shared_boundaries_test_%A_%a.err   # Error log 
 #SBATCH --partition=sesynctest        # queue name, this is for debugging and testing
@@ -12,7 +12,7 @@ pwd; hostname; date
  
 echo "Running shared boundaries code for testing"
  
-INFILE_NAME=sids.shp
+INFILE_NAME=Biomes_disolv.shp
 export INFILE_NAME
 
 Rscript --vanilla /nfs/bparmentier-data/Data/projects/urbanization_effects_on_biodiversity/scripts/shared_length_and_perimeters_polygons_06202017.R
