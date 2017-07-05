65199372bb1cad4403fdc88649cfee7800103bb3
bparment1 <benoit.parmentier@gmail.com>
2017-07-05 14:30:00 -0400

    change to shared boundaries job tested

--- shared_length_boundaries.sh
+++ shared_length_boundaries.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=shared_boundaries_test    # Job name
 #SBATCH --mail-type=ALL                          # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=bparmentier@sesync.org       # Where to send mail
-#SBATCH --time=10:00:00                          # Time limit hrs:min:sec
+#SBATCH --time=02:00:00                          # Time limit hrs:min:sec
 #SBATCH --output=shared_boundaries_test_%A_%a.out   # Standard output from console
 #SBATCH --error=shared_boundaries_test_%A_%a.err   # Error log 
 #SBATCH --partition=sesyncshared        # queue name, this is for debugging and testing
@@ -13,8 +13,8 @@ pwd; hostname; date
 echo "Running shared boundaries code for testing"
  
 INFILE_NAME=Biomes_disolv.shp
-OUT_SUFFIX=urbanization_effects_biodiversity_06212017
+OUT_SUFFIX=urbanization_effects_biodiversity_07052017
 export INFILE_NAME
 export OUT_SUFFIX
 
-Rscript --vanilla /nfs/bparmentier-data/Data/projects/urbanization_effects_on_biodiversity/scripts/shared_length_and_perimeters_polygons_06212017.R
+Rscript --vanilla /nfs/bparmentier-data/Data/projects/urbanization_effects_on_biodiversity/scripts/shared_length_and_perimeters_polygons_07052017.R
